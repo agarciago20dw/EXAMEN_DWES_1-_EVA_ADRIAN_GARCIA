cbd90f7eece3441d9528b27d5364e291086abae6
AdriánDW32 <agarciago20dw@ikzubirimanteo.com>
2021-11-17 08:43:48 +0100

    Modelos hechos

---
+++ app/Models/Paciente.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Paciente extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id',
+        'nombre',
+        'apellidos',
+        'dni',
+        'fecha_nacimiento',
+        'vacunado',
+        'doctor_id',
+    ];
+}
